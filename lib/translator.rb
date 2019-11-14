9fc0bf86c98eacf88078337eace85f9ac6f54bf8
Bretton B Holmes <famunkwriter@me.com>
2019-11-14 00:07:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(filepath, get_emoticon)
     i = 0
     while i < get_emoticon.length do
       if(:get_emoticon[i] == "=D" || :get_emoticon[i] == ":)" || :get_emoticon[i] == ":'(")
-        return meaning[:get_emoticon][value[0]] = value[1]
+        return[:get_emoticon][value[0]] = value[1]
     end
     i += 1
   end

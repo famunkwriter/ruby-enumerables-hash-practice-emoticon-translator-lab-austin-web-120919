2b069a980002f5585f1cdd4bba4a639ded5d7336
Bretton B Holmes <famunkwriter@me.com>
2019-11-14 00:45:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,19 +12,19 @@ def load_library(filepath)
   meaning
 end
 
+
 def get_japanese_emoticon(filepath, get_emoticon)
-  load_library(filepath)
-  translator = YAML.load_file(filepath)
+  translator = load_library(filepath)
   meaning = {get_meaning: {},  get_emoticon: {}}
   translator.each do |key, value|
-
+    binding.pry
     meaning[:get_meaning][value[1]] = key
     meaning[:get_emoticon][value[0]] = value[1]
     i = 0
     while i < translator.length do
       if(:get_emoticon[i] == "=D" || :get_emoticon[i] == ":)" || :get_emoticon[i] == ":'(")
         :get_emoticon[value[0]] = value[1]
-        binding.pry
+        
         return
     end
     i += 1
